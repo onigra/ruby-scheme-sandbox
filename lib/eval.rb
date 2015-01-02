3af2bdeea2e3c08c5dc908802949cb890b91ce9a
onigra <3280467rec@gmail.com>
2015-01-02 17:31:38 +0900

    REPL

--- lib/eval.rb
+++ lib/eval.rb
@@ -91,6 +91,10 @@ module SchemeR
       exp[0] == :quote
     end
 
+    def closure?(exp)
+      exp[0] == :closure
+    end
+
     def primitive_fun?(exp)
       exp[0] == :prim
     end
@@ -322,6 +326,12 @@ module SchemeR
     end
   end
 
+  module Quote
+    def eval_quote(exp, env)
+      car(cdr exp)
+    end
+  end
+
   module Parser
     def parse(exp)
       program = exp.strip().
@@ -333,9 +343,59 @@ module SchemeR
     end
   end
 
-  module Quote
-    def eval_quote(exp, env)
-      car(cdr exp)
+  module Repl
+    def repl
+      prompt = '>>> '
+      second_prompt = '> '
+
+      while true
+        print prompt
+        line = gets or return
+        while line.count('(') > line.count(')')
+          print second_prompt
+          next_line = gets or return
+          line += next_line
+        end
+
+        redo if line =~ /\A\s*\z/m
+
+        begin
+          val = _eval(parse(line), $global_env)
+        rescue Exception => e
+          puts e.to_s
+          redo
+        end
+
+        puts pp(val)
+      end
+    end
+
+    def pp(exp)
+      if exp.is_a?(Symbol) or num?(exp)
+        exp.to_s
+      elsif exp.nil?
+        'nil'
+      elsif exp.is_a?(Array) and closure?(exp)
+        parameter, body, env = exp[1], exp[2], exp[3]
+        "(closure #{pp(parameter)} #{pp(body)} #{pp(env)})"
+      elsif exp.is_a?(Array) and lambda?(exp)
+        parameter, body = exp[1], exp[2]
+        "(lambda #{pp(parameter)} #{pp(body)})"
+      elsif exp.is_a?(Hash)
+        if exp == $primitive_fun_env
+          '*primitive_fun_env*'
+        elsif exp == $boolean_env
+          '*boolean_env*'
+        elsif exp == $list_env
+          '*list_env*'
+        else
+          '{' + exp.map { |k, v| pp(k) + ':' + pp(v) }.join(', ') + '}'
+        end
+      elsif exp.is_a?(Array)
+        '(' + exp.map { |e| pp(e) }.join(', ') + ')'
+      else
+        exp.to_s
+      end
     end
   end
 end
@@ -350,6 +410,8 @@ class Scheme
   include SchemeR::Cons
   include SchemeR::Define
   include SchemeR::Cond
+  include SchemeR::Quote
   include SchemeR::Parser
+  include SchemeR::Repl
 end
 
