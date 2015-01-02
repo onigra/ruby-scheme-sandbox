a6f5e357dae32baec0169bfd02e38be3af6c323b
onigra <3280467rec@gmail.com>
2015-01-02 16:10:10 +0900

    define

--- lib/eval.rb
+++ lib/eval.rb
@@ -234,6 +234,56 @@ module SchemeR
       list
     end
   end
+
+  module Define
+    def eval_define(exp, env)
+      if define_with_parameter?(exp)
+        var, val = define_with_parameter_var_val(exp)
+      else
+        var, val = define_var_val
+      end
+
+      var_ref = lookup_var_ref(var, env)
+
+      if var_ref != nil
+        var_ref[var] = _eval(val, env)
+      else
+        extend_env!([var], [_eval(var, env)], env)
+      end
+
+      nil
+    end
+
+    def extend_env!(parameters, args, env)
+      alist = parameters.zip(args)
+      h = Hash.new
+      alist.each { |k, v| h[k] = v }
+      env.unshift h
+    end
+
+    def define_with_parameter?(exp)
+      list?(exp[1])
+    end
+
+    def define_with_parameter_var_val(exp)
+      var = car(exp[1])
+      parameters, body = cdr(exp[1]), exp[2]
+      val = [:lambda, parameters, body]
+      [var, val]
+    end
+
+    def define_var_val(exp)
+      [exp[1], exp[2]]
+    end
+
+    def lookup_var_ref(var, env)
+      env.find { |alist| alist.key?(var) }
+    end
+
+    def define?(exp)
+      exp[0] == :define
+    end
+  end
 end
 
 class Scheme
@@ -244,5 +294,6 @@ class Scheme
   include SchemeR::If
   include SchemeR::LetRec
   include SchemeR::Cons
+  include SchemeR::Define
 end
 
